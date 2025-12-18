4e7980fcc1e6340580aa9537bfa633ca2ddb8c60
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-18 09:00:58 +0000

    Better messaging

--- talk.py
+++ talk.py
@@ -327,9 +327,9 @@ if __name__ == "__main__":
         print("\tFailed to turn jsonTalkie On!")
         exit(1)
     
-    print(f"\tWARNINGS TO AVOID UNREACHABLE COMMANDS:")
+    print(f"\tWARNINGS TO AVOID UNREACHABLE MESSAGES:")
     print(f"\t\tALWAYS GIVE DIFFERENT MAC ADDRESSES TO YOUR DEVICES DUE TO IP CONFLICTS")
-    print(f"\t\tAVOID BROADCASTED COMMANDS ON WIFI CONNECTED DEVICES DUE TO WIFI RESTRICTIONS")
+    print(f"\t\tAVOID BROADCASTED MESSAGES ON WIFI CONNECTED DEVICES DUE TO WIFI RESTRICTIONS")
     print(f"\t[{cli.manifesto['talker']['name']}] running. Type 'exit' to exit or 'talk' to make them talk.")
     
     try:
